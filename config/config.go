30f8a173b43ac4d8e9b19e5e0e91ae2994371389
Made Rahano Satryani Widhi <76169846+maderahano@users.noreply.github.com>
2022-06-13 19:04:38 +0700

    feat(deploy) : Percobaan Deployment

--- config/config.go
+++ config/config.go
@@ -14,10 +14,10 @@ type Config struct {
 
 func InitConfig() Config {
 	return Config{
-		SERVER_ADDRESS: GetOrDefault("SERVER_ADDRESS", "0.0.0.0:8888"),
-		DB_USERNAME:    GetOrDefault("DB_USERNAME", "root"),
-		DB_PASSWORD:    GetOrDefault("DB_PASSWORD", ""),
-		DB_NAME:        GetOrDefault("DB_NAME", "generate-invoice"),
+		SERVER_ADDRESS: GetOrDefault("SERVER_ADDRESS", "database-generate-invoices.cv8cnjvlnjwz.us-west-1.rds.amazonaws.com"),
+		DB_USERNAME:    GetOrDefault("DB_USERNAME", "admin"),
+		DB_PASSWORD:    GetOrDefault("DB_PASSWORD", "admin12345"),
+		DB_NAME:        GetOrDefault("DB_NAME", "generate_invoices"),
 		DB_PORT:        GetOrDefault("DB_PORT", "3306"),
 		DB_HOST:        GetOrDefault("DB_HOST", "127.0.0.1"),
 		JWT_KEY:        GetOrDefault("JWT_KEY", "AlphaWolf"),
