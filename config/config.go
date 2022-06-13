5fd1dc9db1d057b980c2851ed002cb4c967a62d3
Made Rahano Satryani Widhi <76169846+maderahano@users.noreply.github.com>
2022-06-13 19:33:26 +0700

    fix(database) : wrong host

--- config/config.go
+++ config/config.go
@@ -14,12 +14,12 @@ type Config struct {
 
 func InitConfig() Config {
 	return Config{
-		SERVER_ADDRESS: GetOrDefault("SERVER_ADDRESS", "database-generate-invoices.cv8cnjvlnjwz.us-west-1.rds.amazonaws.com"),
+		SERVER_ADDRESS: GetOrDefault("SERVER_ADDRESS", "0.0.0.0:8888"),
 		DB_USERNAME:    GetOrDefault("DB_USERNAME", "admin"),
 		DB_PASSWORD:    GetOrDefault("DB_PASSWORD", "admin12345"),
 		DB_NAME:        GetOrDefault("DB_NAME", "generate_invoices"),
 		DB_PORT:        GetOrDefault("DB_PORT", "3306"),
-		DB_HOST:        GetOrDefault("DB_HOST", "127.0.0.1"),
+		DB_HOST:        GetOrDefault("DB_HOST", "database-generate-invoices.cv8cnjvlnjwz.us-west-1.rds.amazonaws.com"),
 		JWT_KEY:        GetOrDefault("JWT_KEY", "AlphaWolf"),
 	}
 }
